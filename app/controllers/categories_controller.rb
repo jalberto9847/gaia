bb28aaace0cf4b129bb63d202f3f18724d3f0055
jalberto9847 <josealberto.ch.b@gmail.com>
2019-06-30 18:36:19 -0600

    Adding Edit/Destroy functionalities

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,6 +4,21 @@ class CategoriesController < ApplicationController
     @categories = Category.all
   end
 
+  def edit
+    @category = Category.find(params[:id])
+    @types = Category.types
+  end
+
+  def update
+    @category = Category.find(params[:id])
+
+    if @category.update(category_params)
+      redirect_to @category
+    else
+      render 'edit'
+    end
+  end
+
   def new
     @category = Category.new
     @types = Category.types
@@ -20,10 +35,16 @@ class CategoriesController < ApplicationController
     @category = Category.find(params[:id])
   end
 
+  def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+
+    redirect_to categories_path
+  end
+
   private
 
   def category_params
     params.require(:category).permit(:name, :category_type)
   end
-
 end
